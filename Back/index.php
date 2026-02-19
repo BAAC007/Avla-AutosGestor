eb942e099e2b6ab074389e0f0e405e10303634d5
Bryan Avila <bc714006@gmail.com>
2026-02-19 19:13:28 +0100

    update

--- Back/index.php
+++ Back/index.php
@@ -18,46 +18,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <title>AVLA autosgestor</title>
     <meta charset="utf-8">
-    <style>
-        body,
-        html {
-            width: 100%;
-            height: 100%;
-            padding: 0px;
-            margin: 0px;
-            background: linear-gradient(135deg, #0e1c5a 0%, #2d2f3b 100%);
-        }
-
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-        }
-
-        form {
-            display: flex;
-            flex-direction: column;
-            gap: 20px;
-            padding: 40px;
-            background: #5e6065c6;
-            justify-content: center;
-            align-items: center;
-            border-radius: 10%;
-            width: 250px;
-            height: 250px;
-        }
-
-        form h1{
-            color: white;
-        }
-
-        form input {
-            width: 100%;
-            padding: 10px;
-            box-sizing: border-box;
-            border-radius: 10px;
-        }
-    </style>
 </head>
 
 <body>
